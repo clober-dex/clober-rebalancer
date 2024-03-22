c5cb56d8ea43581b372e0c26ab6d6b9b63a4777f
JChoy <jchoyeclipse@gmail.com>
2024-03-22 10:28:40 +0900

    test: fix book sort problem

--- test/Rebalancer.t.sol
+++ test/Rebalancer.t.sol
@@ -35,8 +35,8 @@ contract RebalancerTest is Test {
         bookManager = new BookManager(address(this), address(0x123), "URI", "URI", "Name", "SYMBOL");
         cloberOpenRouter = new OpenRouter(bookManager);
 
-        tokenA = new MockERC20("Token A", "TKA", 18);
-        tokenB = new MockERC20("Token B", "TKB", 18);
+        MockERC20 tokenA_ = new MockERC20("Token A", "TKA", 18);
+        MockERC20 tokenB_ = new MockERC20("Token B", "TKB", 18);
 
         strategy = new SimpleCouponStrategy(bookManager, address(this));
 
@@ -52,25 +52,25 @@ contract RebalancerTest is Test {
             }
         }
 
-        keyA = IBookManager.BookKey({
-            base: Currency.wrap(address(tokenB)),
+        IBookManager.BookKey memory keyA_ = IBookManager.BookKey({
+            base: Currency.wrap(address(tokenB_)),
             unit: 1e12,
-            quote: Currency.wrap(address(tokenA)),
+            quote: Currency.wrap(address(tokenA_)),
             makerPolicy: FeePolicyLibrary.encode(true, 0),
             hooks: rebalancer,
             takerPolicy: FeePolicyLibrary.encode(true, 0)
         });
-        unopenedKeyA = keyA;
+        unopenedKeyA = keyA_;
         unopenedKeyA.unit = 1e13;
-        keyB = IBookManager.BookKey({
-            base: Currency.wrap(address(tokenA)),
+        IBookManager.BookKey memory keyB_ = IBookManager.BookKey({
+            base: Currency.wrap(address(tokenA_)),
             unit: 1e12,
-            quote: Currency.wrap(address(tokenB)),
+            quote: Currency.wrap(address(tokenB_)),
             makerPolicy: FeePolicyLibrary.encode(true, 0),
             hooks: rebalancer,
             takerPolicy: FeePolicyLibrary.encode(true, 0)
         });
-        unopenedKeyB = keyB;
+        unopenedKeyB = keyB_;
         unopenedKeyB.unit = 1e13;
 
         if (BookId.unwrap(unopenedKeyA.toId()) > BookId.unwrap(unopenedKeyB.toId())) {
@@ -79,14 +79,20 @@ contract RebalancerTest is Test {
             unopenedKeyB = temp;
         }
 
+        key = rebalancer.open(keyA_, keyB_, address(strategy), 3600);
+
+        strategy.setCouponStrategy(key, EpochLibrary.current().add(1), 98534533154674428335, 146389476364791594973); // 4%, 6%
+
+        IRebalancer.Pool memory pool = rebalancer.getPool(key);
+        keyA = bookManager.getBookKey(pool.bookIdA);
+        keyB = bookManager.getBookKey(pool.bookIdB);
+        tokenA = MockERC20(Currency.unwrap(keyA.quote));
+        tokenB = MockERC20(Currency.unwrap(keyB.quote));
+
         tokenA.mint(address(this), 1e27);
         tokenB.mint(address(this), 1e27);
         tokenA.approve(address(rebalancer), type(uint256).max);
         tokenB.approve(address(rebalancer), type(uint256).max);
-
-        key = rebalancer.open(keyA, keyB, address(strategy), 3600);
-
-        strategy.setCouponStrategy(key, EpochLibrary.current().add(1), 98534533154674428335, 146389476364791594973); // 4%, 6%
     }
 
     function testOpen() public {
