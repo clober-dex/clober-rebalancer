c686d80895a72781ea7d325274a962d0b6b38521
JChoy <jchoyeclipse@gmail.com>
2024-07-24 14:59:02 +0900

    test: fix MockSwap to user SafeERC20

--- test/mocks/MockSwap.sol
+++ test/mocks/MockSwap.sol
@@ -2,16 +2,18 @@
 
 pragma solidity ^0.8.20;
 
-import {IERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import {IERC20, SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 contract MockSwap {
+    using SafeERC20 for IERC20;
+
     function swap(address tokenIn, uint256 amountIn, address tokenOut, uint256 amountOut) external {
-        IERC20(tokenIn).transferFrom(msg.sender, address(this), amountIn);
-        IERC20(tokenOut).transfer(msg.sender, amountOut);
+        IERC20(tokenIn).safeTransferFrom(msg.sender, address(this), amountIn);
+        IERC20(tokenOut).safeTransfer(msg.sender, amountOut);
     }
 
     function swapEthForToken(address tokenOut, uint256 amountOut) external payable {
         require(msg.value > 0, "MockSwap: ETH value must be greater than 0");
-        IERC20(tokenOut).transfer(msg.sender, amountOut);
+        IERC20(tokenOut).safeTransfer(msg.sender, amountOut);
     }
 }
