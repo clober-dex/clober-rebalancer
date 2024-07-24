ebc54e72de7f3f9037513d8a644dc2db4bf3d432
JChoy <jchoyeclipse@gmail.com>
2024-07-24 19:13:14 +0900

    feat(deploy): add deploy scripts for Minter and Operator

---
+++ deploy/Operator.ts
@@ -0,0 +1,44 @@
+import { HardhatRuntimeEnvironment } from 'hardhat/types'
+import { DeployFunction } from 'hardhat-deploy/types'
+import { deployWithVerify, SAFE_WALLET, getDeployedAddress } from '../utils'
+import { getChain, isDevelopmentNetwork } from '@nomicfoundation/hardhat-viem/internal/chains'
+import { Address } from 'viem'
+import { arbitrum } from 'viem/chains'
+
+const deployFunction: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const { deployments, getNamedAccounts, network } = hre
+  const chain = await getChain(network.provider)
+  const deployer = (await getNamedAccounts())['deployer'] as Address
+
+  if (await deployments.getOrNull('Operator')) {
+    return
+  }
+
+  let owner: Address = '0x'
+  if (chain.testnet || isDevelopmentNetwork(chain.id)) {
+    owner = deployer
+  } else if (chain.id === arbitrum.id) {
+    owner = SAFE_WALLET[chain.id] // Safe
+  } else {
+    throw new Error('Unknown chain')
+  }
+
+  await deployWithVerify(
+    hre,
+    'Operator',
+    [await getDeployedAddress('SimpleOracleStrategy'), await getDeployedAddress('Rebalancer')],
+    {
+      proxy: {
+        proxyContract: 'UUPS',
+        execute: {
+          methodName: 'initialize',
+          args: [owner],
+        },
+      },
+    },
+  )
+}
+
+deployFunction.tags = ['Operator']
+deployFunction.dependencies = ['Rebalancer', 'SimpleOracleStrategy']
+export default deployFunction
